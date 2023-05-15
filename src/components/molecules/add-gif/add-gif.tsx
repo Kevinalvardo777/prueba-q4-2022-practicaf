ff0599c5e00da2a648f0f3d29fc0ac8d1be1f2b4
Esteban <esteban.fernando07@gmail.com>
2023-05-15 12:20:45 -0500

    fixed styles

--- src/components/molecules/add-gif/add-gif.tsx
+++ src/components/molecules/add-gif/add-gif.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
-import { Input } from '../../atoms/input/input'
-import { Button } from '../../atoms/button/button'
-import { useAddGif } from './use-add-gif/use-add-gif'
-import './add-gif.scss'
+import React, { useState } from "react";
+import { Input } from "../../atoms/input/input";
+import { Button } from "../../atoms/button/button";
+import { useAddGif } from "./use-add-gif/use-add-gif";
+import "./add-gif.scss";
 
 export const AddGif = () => {
+  const { onChange, handleSubmit, inputValue, errorMessage } = useAddGif();
 
-    const {onChange, handleSubmit, inputValue, errorMessage} = useAddGif()
-
-    return (
-        
-        <div className='add-gif add-gif__wrapper'>
-            <h2 className='add-gif add-gif__wrapper__title'>Gif Card</h2>
-            <section className='add-gif add-gif__wrapper__add'>
-                <Input value={inputValue} errorMessage={errorMessage} onChange={onChange} placeholder='Gift URL'/>
-                <Button onClick={handleSubmit}>Agregar</Button>
-            </section>
-        </div>
-        
-    )
-
-}
+  return (
+    <div className="add-gif add-gif__wrapper">
+      <h2 className="add-gif add-gif__wrapper__title">Gif Gallery</h2>
+      <section className="add-gif add-gif__wrapper__add">
+        <Input
+          value={inputValue}
+          errorMessage={errorMessage}
+          onChange={onChange}
+          placeholder="Gift URL"
+        />
+        <Button onClick={handleSubmit}>Agregar</Button>
+      </section>
+    </div>
+  );
+};
--- src/components/organisms/card-grid/card-grid.tsx
+++ src/components/organisms/card-grid/card-grid.tsx
@@ -1,16 +1,20 @@
-import React, {PropsWithChildren, FC} from 'react'
-import './card-grid.scss'
+import React, { PropsWithChildren, FC } from "react";
+import "./card-grid.scss";
 import { Gif } from "../../../interfaces/gif";
-import { GifCard } from '../../molecules/gif-card/gif-card'
+import { GifCard } from "../../molecules/gif-card/gif-card";
 
 interface CardGridProps extends PropsWithChildren {
-  gifs: Gif[]
+  gifs: Gif[];
 }
 
-export const CardGrid: FC<CardGridProps> = ({gifs}) => {
+export const CardGrid: FC<CardGridProps> = ({ gifs }) => {
   return (
-    <div className='card-grid card-grid__wrapper'>
-      {gifs.map(({id, url})=><GifCard id={id} url={url}/>)}
+    <div className="card-grid card-grid__wrapper">
+      <div className="card-grip card-grid__grid">
+        {gifs.map(({ id, url }) => (
+          <GifCard id={id} url={url} key={id} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
